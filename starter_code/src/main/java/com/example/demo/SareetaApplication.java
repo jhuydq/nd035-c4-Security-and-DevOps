7150df4bbae06715f0837b0013c2638ebeef7c73
davidinh <quochuyd@gmail.com>
2021-02-22 18:33:29 +0700

    test push

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SareetaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test Git");
 		SpringApplication.run(SareetaApplication.class, args);
 	}
 
