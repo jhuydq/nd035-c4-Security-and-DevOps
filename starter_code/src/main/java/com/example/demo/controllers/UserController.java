33542f0e585ae5900b83c4ba2c387a2f61cc3b17
davidinh <quochuyd@gmail.com>
2021-02-25 09:37:41 +0700

    add some test cases

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -5,14 +5,19 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -38,6 +43,13 @@ public class UserController {
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
+
+
+		log.info("username set info : {}", createUserRequest.getUsername());
+		log.warn("username set warn : {}", createUserRequest.getUsername());
+		log.debug("username set debug : {}",createUserRequest.getUsername());
+		log.error("username set error : {}", createUserRequest.getUsername());
+
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
